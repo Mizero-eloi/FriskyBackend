f76f3190867109732a99e986997e854eccc342b0
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-01-06 17:30:26 +0200

    joinChallengeCommits

--- controllers/challengeController.js
+++ controllers/challengeController.js
@@ -49,11 +49,13 @@ module.exports.postChallengeVideoWhileMakingChallenge = async (req, res, next) =
                 "challenger.challengeVideo": req.file.path
             }
         }, {new: true});
+        
+        if(!challenge) return res.status(400).send("Challenge not found !");
     
         res.status(200).send(challenge);
 
     }catch(ex){
-        res.status(500).send("Something went wrong");
+        res.status(500).send(ex.message); 
         console.log(ex);
     }
 
@@ -79,4 +81,38 @@ module.exports.uploadChallengeCoverPhoto = async (req, res, next) => {
 }
 
 
+module.exports.joinChallenge = async (req, res, next) => { 
+    try{
+    // Checking if the logged user is in our database
+    const participant = await User.findById(req.user._id).select({username: 1, profile: 1, _id: 1});
+    if(!participant) return res.status(400).send("User does not exist !");
 
+
+    // checking if the given challenge exists 
+    let challenge = await Challenge.findById(req.params.challengeId);
+    if(!challenge) return res.status(400).send("Challenge does not exist !");
+    
+    // Checking if the user is not already in the competition 
+    const competitor = challenge.participants.length > 0 && challenge.participants.filter(p => p._id == participant._id);
+    if(competitor) return res.status(400).send("You are already a competitor");
+
+    // Adding the participant to the competition
+
+    challenge.participants.push({
+            name: participant.username || "eloimizero",
+            profile: participant.profile || "eloi's profile",
+            challengeVideo: req.file.path
+        })
+
+    await challenge.save()
+    
+    
+    res.status(200).send(challenge.participants); 
+
+    }catch(ex){
+        res.status(500).send("Something went wrong");
+        console.log(ex);
+    }
+
+
+}
