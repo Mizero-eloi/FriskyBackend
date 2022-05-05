26353bdd53a1f794b57795458d65deaad4f74337
n-jabes <nshutij7@gmail.com>
2022-05-05 15:52:08 +0200

    Finished working on the search api for both user and challenge

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const { isEmpty } = require("lodash");
 const { User } = require("../models/User");
 const { getAllDocuments } = require("../services/queries");
 
@@ -8,4 +9,24 @@ module.exports.getAllUsers = async (req, res) => {
     fields: { password: 0 },
     res: res,
   });
+};
+
+//searching for user
+module.exports.searchUser = async (req, res, next) => {
+  //user input
+  console.log(req.body.UserToSearch);
+  //searching for the user
+  let data = await User.find(
+    {
+      "$or": [
+        {username: {$regex: req.body.UserToSearch}}
+      ]
+    });
+
+    if(!isEmpty(data)){
+      return res.status(200).send(data); 
+    }else{
+      return res.status(400).send("No user with such username");
+    }
+
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const { getAllUsers } = require("../controllers/userController");
+const auth = require("../middleware/auth");
+const { 
+    getAllUsers,
+    searchUser } = require("../controllers/userController");
 const router = express.Router();
 
 router.get("/", getAllUsers);
+router.post(
+    "/searchUser", 
+    auth, 
+    searchUser
+);
 
 module.exports = router;
